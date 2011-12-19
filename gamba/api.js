ad0e9955503f2da171413bb792844aea33119758
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-12-19 16:03:55 -0200

    create menu element

--- gamba/api.js
+++ gamba/api.js
@@ -2,12 +2,43 @@ function Action(name, doAction) {
     var self = this;
     self.name = name;
     self.doAction = doAction;
+    self.element = $("<button />").text(self.name);
 };
 
 function Menu(actions) {
     var self = this;
     self.actions = actions;
-    var element = $("<div />").attr({
-        id: "gamba_menu"
+    self.show = function() {
+        self.hide();
+        $(actions).each(function(i, action) {
+            action.element.click(action.doAction);
+            gamba_menu.append(action.element);
+        });
+        gamba_menu.show();
+    };
+    self.hide = function() {
+        gamba_menu.children().remove();
+    };
+    $(document).click(function() {
+        self.hide();
     });
 };
+
+jQuery.fn.setMenu = function(menu) {
+    this.contextmenu(function(event) {
+        gamba_menu.css({
+            position: "absolute",
+            left: event.pageX,
+            top: event.pageY
+        });
+        menu.show();
+        return false;
+    });
+};
+
+$(document).ready(function() {
+    gamba_menu = $("<div />").attr({
+        id: "gamba_menu"
+    });
+    $(document.body).append(gamba_menu);
+});
--- gamba/tests.js
+++ gamba/tests.js
@@ -29,3 +29,36 @@ test("it has actions", function() {
     equal(menu.actions[0], action1);
     equal(menu.actions[1], action2);
 });
+
+test("it creates an element on page", function() {
+    var action1 = new Action("action1");
+    var action2 = new Action("action2");
+    equal($("div#gamba_menu").length, 1);
+    var menu = new Menu(actions=[
+        action1,
+        action2
+    ]);
+    equal($("div#gamba_menu").length, 1);
+    equal($("div#gamba_menu").children().length, 0);
+    menu.show();
+    equal($("div#gamba_menu").children().length, 2);
+    menu.hide();
+    equal($("div#gamba_menu").children().length, 0);
+});
+
+test("it binds to an element", function() {
+    var element = $("<p />").text("element test");
+    var action1 = new Action("action1");
+    var action2 = new Action("action2");
+    var menu = new Menu(actions=[
+        action1,
+        action2
+    ]);
+    element.setMenu(menu);
+    $(document.body).append(element);
+    equal($("div#gamba_menu").children().length, 0);
+    element.contextmenu();
+    equal($("div#gamba_menu").children().length, 2);
+    menu.hide();
+    element.remove();
+});
